b57ed40b8981577f3b6d4cf63d70aca349261a6d
Steve K <steve@actor.im>
2015-12-22 20:01:27 +0300

    Release v1.0.58

--- build/components/dialog/messages/Modern.react.js
+++ build/components/dialog/messages/Modern.react.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _lodash = require('lodash');
 
 var _react = require('react');
 
@@ -24,40 +28,205 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 * Copyright (C) 2015 Actor LLC. <https://actor.im>
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 */
 
+var ColorTypes = {
+  HEX: 'hex',
+  NAMED: 'named'
+};
+
+/**
+ * @param color
+ * @returns {string | null} Color value
+ */
+function getColor(color) {
+  if (color) {
+    switch (color.type) {
+      case ColorTypes.HEX:
+        return color.hex;
+      case ColorTypes.NAMED:
+        return color.name;
+      default:
+        return null;
+    }
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Class that represents component for display modern text message attachment field
+ * @param {string} title Field title
+ * @param {string} value Field value
+ * @param {bool} isShort Display short version of field
+ */
+
+var Field = (function (_Component) {
+  _inherits(Field, _Component);
+
+  function Field(props) {
+    _classCallCheck(this, Field);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Field).call(this, props));
+    //console.debug('Field', props);
+  }
+
+  _createClass(Field, [{
+    key: 'render',
+    value: function render() {
+      var _props = this.props;
+      var title = _props.title;
+      var value = _props.value;
+      var isShort = _props.isShort;
+
+      var fieldClassName = (0, _classnames2.default)('field', {
+        'field--short': isShort,
+        'col-xs-6': isShort,
+        'col-xs-12': !isShort
+      });
+
+      return _react2.default.createElement(
+        'div',
+        { className: fieldClassName },
+        title ? _react2.default.createElement(
+          'div',
+          { className: 'field__title' },
+          title
+        ) : null,
+        value ? _react2.default.createElement(
+          'div',
+          { className: 'field__value' },
+          value
+        ) : null
+      );
+    }
+  }]);
+
+  return Field;
+})(_react.Component);
+
+/**
+ * Class that represents component for display modern text message attachments
+ * @param {array} fields Array of objects contains attachment fields
+ * @param {object} paragraphStyle Contains attachment styles
+ * @param {string} text Attachment text
+ * @param {string} title Attachment title
+ * @param {string} titleUrl Attachment title url
+ */
+
+Field.propTypes = {
+  title: _react.PropTypes.string.isRequired,
+  value: _react.PropTypes.string.isRequired,
+  isShort: _react.PropTypes.bool.isRequired
+};
+
+var Attach = (function (_Component2) {
+  _inherits(Attach, _Component2);
+
+  function Attach(props) {
+    _classCallCheck(this, Attach);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Attach).call(this, props));
+    //console.debug('Attach', props);
+  }
+
+  _createClass(Attach, [{
+    key: 'render',
+    value: function render() {
+      var _props2 = this.props;
+      var paragraphStyle = _props2.paragraphStyle;
+      var titleUrl = _props2.titleUrl;
+      var title = _props2.title;
+      var text = _props2.text;
+      var fields = _props2.fields;
+
+      var attachmentClassName = (0, _classnames2.default)('attachment', {
+        'attachment--paragraph': paragraphStyle.showParagraph
+      });
+
+      var attachmentStyles = {
+        borderColor: getColor(paragraphStyle.color) || 'transparent',
+        backgroundColor: getColor(paragraphStyle.bgColor) || 'transparent'
+        //color: getColor(paragraphStyle.color) || 'inherit'
+      };
+
+      var visibleTitle = titleUrl ? _react2.default.createElement(
+        'a',
+        { href: titleUrl },
+        title
+      ) : { title: title };
+
+      var attachmentFields = (0, _lodash.map)(fields, function (field, index) {
+        return _react2.default.createElement(Field, _extends({ key: index }, field));
+      });
+
+      return _react2.default.createElement(
+        'div',
+        { className: attachmentClassName, style: attachmentStyles },
+        _react2.default.createElement(
+          'div',
+          { className: 'attachment__title' },
+          visibleTitle
+        ),
+        text ? text : null,
+        attachmentFields ? _react2.default.createElement(
+          'div',
+          { className: 'attachment_fields row' },
+          attachmentFields
+        ) : null
+      );
+    }
+  }]);
+
+  return Attach;
+})(_react.Component);
+
 /**
  * Class that represents component for display modern text messages content
+ * @param {array} attaches Array of objects contains modern message attached paragraphs
  * @param {object} paragraphStyle Contains message styles
  * @param {string} text Message text
  * @param {string} className Component class name
  */
 
-var TextModern = (function (_Component) {
-  _inherits(TextModern, _Component);
+Attach.propTypes = {
+  paragraphStyle: _react.PropTypes.object.isRequired,
+  text: _react.PropTypes.string.isRequired,
+  title: _react.PropTypes.string.isRequired,
+  titleUrl: _react.PropTypes.string.isRequired,
+  fields: _react.PropTypes.array.isRequired
+};
+
+var TextModern = (function (_Component3) {
+  _inherits(TextModern, _Component3);
 
   function TextModern(props) {
     _classCallCheck(this, TextModern);
 
     return _possibleConstructorReturn(this, Object.getPrototypeOf(TextModern).call(this, props));
+    //console.debug('Modern', props);
   }
 
   _createClass(TextModern, [{
     key: 'render',
     value: function render() {
-      var _props = this.props;
-      var paragraphStyle = _props.paragraphStyle;
-      var attaches = _props.attaches;
-      var text = _props.text;
-      var className = _props.className;
+      var _props3 = this.props;
+      var paragraphStyle = _props3.paragraphStyle;
+      var attaches = _props3.attaches;
+      var text = _props3.text;
+      var className = _props3.className;
 
       var modernClassName = (0, _classnames2.default)('modern', {
         'modern--paragraph': paragraphStyle.showParagraph
       });
 
       var modernStyles = {
-        borderColor: paragraphStyle.color ? paragraphStyle.color.name : 'transparent',
-        backgroundColor: paragraphStyle.bgColor ? paragraphStyle.bgColor.name : 'transparent',
-        color: paragraphStyle.color ? paragraphStyle.color.name : 'inherit'
+        borderColor: getColor(paragraphStyle.color) || 'transparent',
+        backgroundColor: getColor(paragraphStyle.bgColor) || 'transparent'
+        //color: getColor(paragraphStyle.color) || 'inherit'
       };
+
+      var modernAttachments = (0, _lodash.map)(attaches, function (attachment, index) {
+        return _react2.default.createElement(Attach, _extends({ key: index }, attachment));
+      });
 
       return _react2.default.createElement(
         'div',
@@ -65,11 +234,12 @@ var TextModern = (function (_Component) {
         _react2.default.createElement(
           'div',
           { className: modernClassName, style: modernStyles },
-          paragraphStyle.showParagraph ? _react2.default.createElement(
+          text ? _react2.default.createElement(
             'p',
             null,
             text
-          ) : { text: text }
+          ) : null,
+          modernAttachments
         )
       );
     }
@@ -79,7 +249,7 @@ var TextModern = (function (_Component) {
 })(_react.Component);
 
 TextModern.propTypes = {
-  attaches: _react.PropTypes.object.isRequired,
+  attaches: _react.PropTypes.array.isRequired,
   paragraphStyle: _react.PropTypes.object.isRequired,
   text: _react.PropTypes.string.isRequired,
   className: _react.PropTypes.string
--- src/components/dialog/messages/Modern.react.js
+++ src/components/dialog/messages/Modern.react.js
@@ -2,18 +2,131 @@
  * Copyright (C) 2015 Actor LLC. <https://actor.im>
  */
 
+import { map } from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 
+const ColorTypes = {
+  HEX: 'hex',
+  NAMED: 'named'
+};
+
+/**
+ * @param color
+ * @returns {string | null} Color value
+ */
+function getColor(color) {
+  if (color) {
+    switch (color.type) {
+      case ColorTypes.HEX:
+        return color.hex;
+      case ColorTypes.NAMED:
+        return color.name;
+      default:
+        return null;
+    }
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Class that represents component for display modern text message attachment field
+ * @param {string} title Field title
+ * @param {string} value Field value
+ * @param {bool} isShort Display short version of field
+ */
+class Field extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    isShort: PropTypes.bool.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    //console.debug('Field', props);
+  }
+
+  render() {
+    const { title, value, isShort } = this.props;
+
+    const fieldClassName = classnames('field', {
+      'field--short': isShort,
+      'col-xs-6': isShort,
+      'col-xs-12': !isShort
+    });
+
+    return (
+      <div className={fieldClassName}>
+        {title ? <div className="field__title">{title}</div> : null}
+        {value ? <div className="field__value">{value}</div> : null}
+      </div>
+    )
+  }
+}
+
+/**
+ * Class that represents component for display modern text message attachments
+ * @param {array} fields Array of objects contains attachment fields
+ * @param {object} paragraphStyle Contains attachment styles
+ * @param {string} text Attachment text
+ * @param {string} title Attachment title
+ * @param {string} titleUrl Attachment title url
+ */
+class Attach extends Component {
+  static propTypes = {
+    paragraphStyle: PropTypes.object.isRequired,
+    text: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    titleUrl: PropTypes.string.isRequired,
+    fields: PropTypes.array.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    //console.debug('Attach', props);
+  }
+
+  render() {
+    const { paragraphStyle, titleUrl, title, text, fields } = this.props;
+
+    const attachmentClassName = classnames('attachment', {
+      'attachment--paragraph': paragraphStyle.showParagraph
+    });
+
+    const attachmentStyles = {
+      borderColor: getColor(paragraphStyle.color) || 'transparent',
+      backgroundColor: getColor(paragraphStyle.bgColor) || 'transparent'
+      //color: getColor(paragraphStyle.color) || 'inherit'
+    };
+
+    const visibleTitle = titleUrl ? <a href={titleUrl}>{title}</a> : {title};
+
+    const attachmentFields = map(fields, (field, index) => <Field key={index} {...field}/>);
+
+    return (
+      <div className={attachmentClassName} style={attachmentStyles}>
+        <div className="attachment__title">
+          {visibleTitle}
+        </div>
+        {text ? text : null}
+        {attachmentFields ? <div className="attachment_fields row">{attachmentFields}</div> : null}
+      </div>
+    );
+  }
+}
+
 /**
  * Class that represents component for display modern text messages content
+ * @param {array} attaches Array of objects contains modern message attached paragraphs
  * @param {object} paragraphStyle Contains message styles
  * @param {string} text Message text
  * @param {string} className Component class name
  */
 class TextModern extends Component {
   static propTypes = {
-    attaches: PropTypes.object.isRequired,
+    attaches: PropTypes.array.isRequired,
     paragraphStyle: PropTypes.object.isRequired,
     text: PropTypes.string.isRequired,
     className: PropTypes.string
@@ -21,6 +134,7 @@ class TextModern extends Component {
 
   constructor(props) {
     super(props);
+    //console.debug('Modern', props);
   }
 
   render() {
@@ -30,19 +144,18 @@ class TextModern extends Component {
     });
 
     const modernStyles = {
-      borderColor: paragraphStyle.color ? paragraphStyle.color.name : 'transparent',
-      backgroundColor: paragraphStyle.bgColor ? paragraphStyle.bgColor.name : 'transparent',
-      color: paragraphStyle.color ? paragraphStyle.color.name : 'inherit'
+      borderColor: getColor(paragraphStyle.color) || 'transparent',
+      backgroundColor: getColor(paragraphStyle.bgColor) || 'transparent'
+      //color: getColor(paragraphStyle.color) || 'inherit'
     };
+
+    const modernAttachments = map(attaches, (attachment, index) => <Attach key={index} {...attachment}/>);
 
     return (
       <div className={className}>
         <div className={modernClassName} style={modernStyles}>
-          {
-            paragraphStyle.showParagraph
-              ? <p>{text}</p>
-              : {text}
-          }
+          {text ? <p>{text}</p> : null}
+          {modernAttachments}
         </div>
       </div>
     );
